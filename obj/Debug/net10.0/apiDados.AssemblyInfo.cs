915675dc90ceca127a1f9af81fe93a46ab69a312
Gustavo Costa da Silva <gustavocostabox@gmail.com>
2026-09-22 00:51:47 -0300

    collection do Bruno

--- obj/Debug/net10.0/apiDados.AssemblyInfo.cs
+++ obj/Debug/net10.0/apiDados.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("apiDados")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76f0044386413993f0907d416e6800d383615743")]
 [assembly: System.Reflection.AssemblyProductAttribute("apiDados")]
 [assembly: System.Reflection.AssemblyTitleAttribute("apiDados")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
